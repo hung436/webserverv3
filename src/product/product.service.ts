aa52aa5c46c27415edace70cb3114051e97f9226
hung436 <ttldhung@gmail.com>
2022-10-26 10:03:02 +0700

    [feat] send mail

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,23 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import console from 'console';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { Image } from 'src/cloudinary/entities/image.entity';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { ProductToSize } from './entities/productToSize.entity';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product) private productRepository: Repository<Product>,
+    @InjectRepository(Image) private imageRepository: Repository<Image>,
+    @InjectRepository(ProductToSize)
+    private proToSizeRepository: Repository<ProductToSize>,
+    private CloudinaryService: CloudinaryService,
   ) {}
-  create(createProdutDto: CreateProductDto): Promise<object> {
+  create(
+    product: CreateProductDto,
+    files: Express.Multer.File[],
+  ): Promise<object> {
     return new Promise<object>(async (resolve) => {
       try {
-        await this.productRepository.create({
-          name: createProdutDto.name,
-          image: createProdutDto.image,
+        const check = await this.productRepository.findOne({
+          where: { name: product.name },
         });
-        resolve({ success: true, message: 'Create product successfully' });
+        if (!check) {
+          const images: any = [];
+          for (const file of files) {
+            const result = await this.CloudinaryService.uploadImage(file);
+            const image = new Image();
+            image.imageLink = result.secure_url;
+            image.publicId = result.public_id;
+            await this.imageRepository.save(image);
+            images.push(image);
+          }
+          const newProductToSize = new ProductToSize();
+          newProductToSize.price = parseInt(product.price);
+          newProductToSize.sizeId = product.size;
+          await this.proToSizeRepository.save(newProductToSize);
+
+          const newProduct = new Product();
+
+          newProduct.name = product.name;
+          newProduct.promotionPrice = product.promotionPrice;
+          newProduct.description = product.description;
+          newProduct.quantity = product.quantity;
+          newProduct.title = product.title;
+          newProduct.images = images;
+          newProduct.productToSizes = [newProductToSize];
+
+          await this.productRepository.save(newProduct);
+          resolve({ success: true, message: 'Create product successfully' });
+        } else {
+          resolve({ success: false, message: 'Name already exists' });
+        }
       } catch (error) {
         throw new Error(error);
       }
@@ -25,7 +64,9 @@ export class ProductService {
   }
 
   findAll() {
-    return this.productRepository.find();
+    return this.productRepository.find({
+      relations: { images: true, category: true, productToSizes: true },
+    });
   }
 
   findOne(id: number) {
@@ -36,7 +77,7 @@ export class ProductService {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove() {
+    return;
   }
 }
